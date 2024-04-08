a22601bf10b13d3e4947c29b275279b5dfdc9261
Vishal Gupta <vishal.gupta@kellton.com>
2024-04-08 10:59:23 +0530

    Post and Pre Increment operator

---
+++ src/IncrementPrePost.kt
@@ -0,0 +1,17 @@
+fun main(){
+
+    // Post Increment
+    // First uses the value and the increments it
+    var i = 11
+    println(i++)
+
+    //Post Increment
+    // First increments the value and the uses it
+    var j = 15
+    println(++j)
+
+    // Task- calculate output
+    var a = 10
+
+    println(a++ + ++a + a++)   // 10 + 12 + 12 = 34
+}
