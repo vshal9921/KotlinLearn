0346ae89a2dfbef2fd8a19c40ade704b0f675172
Vishal Gupta <vishal.gupta@kellton.com>
2024-04-09 10:39:07 +0530

    Setting default value of parameter in function

---
+++ src/FunctionDefaultArg.kt
@@ -0,0 +1,8 @@
+fun main() = printMsg()
+
+fun printMsg(count : Int = 2){  // The default value of param count is defined as 2even if param is not passed during function call
+
+    for (i in 1..count){
+        println("Hello $i")
+    }
+}
