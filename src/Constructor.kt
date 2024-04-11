24ac80d89632ce171a7655fbf6a9816bfd33bc4d
Vishal Gupta <vishal.gupta@kellton.com>
2024-04-11 14:42:19 +0530

    init block, primary and secondary constructors

---
+++ src/Constructor.kt
@@ -0,0 +1,23 @@
+fun main(){
+
+    var person1 = Person("A", 20)
+    println(person1.name)
+    println(person1.age)
+
+    var person2 = Person("B")
+    println(person2.name)
+    println(person2.age)
+}
+
+// Order of execution - primary constructors - init blocks - secondary constructors
+
+class Person(var name: String, val age: Int){
+
+    init {
+        println("init block initialized")
+    }
+
+    constructor(perName : String) : this(perName, 30){
+        println("secondary constructor initialized")
+    }
+}
