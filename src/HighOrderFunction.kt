ab861e0ad4d519a33e04cb94089458a493b7f30f
Vishal Gupta <vishal.gupta@kellton.com>
2024-04-15 15:16:51 +0530

    High order function

---
+++ src/HighOrderFunction.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var fn: (a: Double, b: Double) -> Double = ::addition
+    println(fn(2.0, 4.0))
+    calculator(2.78, 4.0, ::addition)
+}
+
+fun addition(a: Double,b: Double) : Double {
+    return a + b
+}
+
+fun calculator(a: Double,b : Double, gn : (Double, Double) -> Double) {
+
+    var result = gn(a,b)
+    println(result)
+}
