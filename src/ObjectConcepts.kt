9d0eb0c938be7d3e86d12e7c9f0f6becf00232fe
Vishal Gupta <vishal.gupta@kellton.com>
2024-04-15 10:27:10 +0530

    object keyword concepts

---
+++ src/ObjectConcepts.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    var obj = object : ObjectConcepts() {  // used for singleTon, initialized only once
+
+        override fun run(){
+            super.run()
+            println("run method in object")
+        }
+    }
+
+    obj.run()
+}
+
+open class ObjectConcepts {
+
+    open fun run(){
+        println("run method in objectConcepts")
+    }
+}
