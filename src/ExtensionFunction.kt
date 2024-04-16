99e026e6ab87e21d5eaa420dfa00a9b71efc04b8
Vishal Gupta <vishal.gupta@kellton.com>
2024-04-16 11:12:40 +0530

    Extension function

---
+++ src/ExtensionFunction.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    println("This is it".formattedString())
+}
+
+// Extension fuction is a function which can be added in another class
+// Here formattedString() has been added in String class and can be used with all objects of String class
+// private variables of class can not be accessed in extension functions
+fun String.formattedString() : String{
+    return "------------------------\n $this \n -------------------------------"
+}
