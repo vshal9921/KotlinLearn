315a1de597819e6ce76d6dcd3e65cd6d9923806d
Vishal Gupta <vishal.gupta@kellton.com>
2024-04-16 12:50:22 +0530

    Generic concept

---
+++ src/Generic.kt
@@ -0,0 +1,33 @@
+fun main(){
+
+    var intContainer = IntContainer(67)
+    println(intContainer.getValue())
+
+
+    var container = Container<String>("this is it")
+    println(container.getValue())
+}
+
+// Generic class which can return any data type object
+class Container<T>(var data : T){
+
+    fun getValue() : T{
+        return data
+    }
+
+    fun setValue(data: T){
+        this.data = data
+    }
+}
+
+class IntContainer(var data : Int){
+
+    fun getValue() : Int{
+        return data
+    }
+
+    fun setValue(data : Int){
+        this.data = data
+    }
+
+}
