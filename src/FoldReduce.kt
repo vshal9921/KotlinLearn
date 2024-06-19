fe376636b6a37866a0c1b47c47e7f5b60756519a
Vishal Gupta <vshal9921@gmail.com>
2024-06-19 14:24:24 +0530

    Fold and Reduce - Collections

---
+++ src/FoldReduce.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    val numbers = listOf(3, 6, 9, 14)
+
+    val sumFold = numbers.fold(0){ acc, number -> acc + number}
+
+    println("Sum using fold is $sumFold")
+
+    val sumReduce = numbers.reduce{acc, number -> acc + number}
+
+    println("Sum using reduce is $sumReduce")
+
+}
